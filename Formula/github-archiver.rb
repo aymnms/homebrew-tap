ff7a0ef2714f7150d97183c957604a2a1a5716cf
aymnms <micheletaymerick@gmail.com>
2026-06-18 01:34:18 +0200

    feat: bump github-archiver to v1.0.4

--- Formula/github-archiver.rb
+++ Formula/github-archiver.rb
@@ -1,8 +1,8 @@
 class GithubArchiver < Formula
   desc "Mirror GitHub repositories to another account or organization"
   homepage "https://github.com/aymnms/github-archiver"
-  url "https://github.com/aymnms/github-archiver/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "48c4c654794d7ab416274a28f99bcd7bbbbe8fed0d54652ee94d6708ff1ac13c"
+  url "https://github.com/aymnms/github-archiver/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "f847fad6546c72c15b1e4879f431289cd25cfd9da9fd89cf03d6d88174fca6da"
   license "MIT"
 
   def install
